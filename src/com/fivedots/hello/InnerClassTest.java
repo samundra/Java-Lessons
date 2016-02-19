754987509d3812fa48010e6c94d20edf8ef68b9d
Samundra Shrestha <samundra.shr@gmail.com>
2016-02-18 21:56:06 -0800

    update code for InnterClassTest
    show the sample usage of inner class

--- src/com/fivedots/hello/InnerClassTest.java
+++ src/com/fivedots/hello/InnerClassTest.java
@@ -1,5 +1,54 @@
 package com.fivedots.hello;
 
-public class InnerClassTest {
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 
+import javax.swing.Timer;
+import javax.swing.JOptionPane;
+
+public class InnerClassTest {
+	public static void main(String[] args){
+		TalkingClock clock = new TalkingClock(4000, true);
+		clock.start();
+		
+		// keep program running until user selects "OK"
+		JOptionPane.showMessageDialog(null,  "Quit Program");
+		System.exit(0);
+		
+	}
+}
+
+class TalkingClock
+{
+	public TalkingClock(int interval, boolean beep)
+	{
+		this.interval = interval;
+		this.beep = beep;
+		
+	}
+	
+	public void start(){
+		ActionListener listener = new TimePrinter();
+		Timer t = new Timer(interval, listener);
+		t.start();
+	}
+	
+	private int interval;
+	private boolean beep;
+	
+	public class TimePrinter implements ActionListener
+	{
+
+		
+		public void actionPerformed(ActionEvent event) {
+			
+			Date now = new Date();
+			System.out.println("At the tone, the time is "+now);
+			if(beep) Toolkit.getDefaultToolkit().beep();
+			
+		}
+		
+	}
 }
