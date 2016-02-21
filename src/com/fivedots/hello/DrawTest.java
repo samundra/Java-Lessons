416c8a0b0f70bc99b7e57a287931145f45550a24
Samundra Shrestha <samundra.shr@gmail.com>
2016-02-21 00:50:57 -0800

    draw various shapes using Graphics2D

--- src/com/fivedots/hello/DrawTest.java
+++ src/com/fivedots/hello/DrawTest.java
@@ -1,18 +1,23 @@
 package com.fivedots.hello;
 
 import java.awt.EventQueue;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
-
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
+/**
+ * @version 1.32 2007-04-14
+ * @author Cay Horstmann
+ *
+ */
 public class DrawTest {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			
-			@Override
 			public void run() {
 					DrawFrame frame = new DrawFrame();
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,6 +28,10 @@ public class DrawTest {
 }
 
 
+/**
+ * A frame that contains a panel with drawings
+ *
+ */
 class DrawFrame extends JFrame
 {
 	public DrawFrame()
@@ -42,9 +51,12 @@ class DrawFrame extends JFrame
 	
 }
 
+/**
+ * A component that displays rectangles and ellipses.
+ */
 class DrawComponent extends JComponent
 {
-	public void paintComponent(Graphics2D g)
+	public void paintComponent(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D) g;
 		
@@ -57,8 +69,21 @@ class DrawComponent extends JComponent
 		Rectangle2D rect = new Rectangle2D.Double(leftX, topY, width, height);
 		g2.draw(rect);
 		
+		Ellipse2D ellipse = new Ellipse2D.Double();
+		ellipse.setFrame(rect);;
+		g2.draw(ellipse);
+		
 		// draw a diagonal line
 		g2.draw(new Line2D.Double(leftX, topY, leftX+width, topY+height));
+		
+		// Draw a circle with same center
+		double centerX = rect.getCenterX();
+		double centerY = rect.getCenterY();
+		double radius = 150;
+		
+		Ellipse2D circle = new Ellipse2D.Double();
+		circle.setFrameFromCenter(centerX, centerY, centerX+radius, centerY+radius);
+		g2.draw(circle);
 		
 	}
 }
