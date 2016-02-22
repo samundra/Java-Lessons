2c78314ef5955f743c78696c7bc9db84abb1dc40
Samundra Shrestha <samundra.shr@gmail.com>
2016-02-21 22:09:59 -0800

    add window events
    add window events and make use of WindowAdapter class. WindowAdapter
    class implements all of seven methods required by WindowListener. We can
    just override the method that we are interested in and the rest method
    are implemetned as "do nothing" by WindowAdapter class.

--- src/com/fivedots/hello/LookAndFeelTest.java
+++ src/com/fivedots/hello/LookAndFeelTest.java
@@ -3,6 +3,8 @@ package com.fivedots.hello;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,8 +22,19 @@ public class LookAndFeelTest {
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setVisible(true);
 				
+				frame.addWindowListener(new Terminator());
+				
 			}
 		});
+	}
+}
+
+class Terminator extends WindowAdapter{
+	public void windowClosing(WindowEvent e){
+		if(true){
+			System.out.print("Exit system.");
+			System.exit(0);
+		}
 	}
 }
 
