c577b141f7edae6657bd62feeb1ae48c5e1490dc
Samundra Shrestha <samundra.shr@gmail.com>
2016-02-21 22:00:05 -0800

    change look and feel of java GUI app dynamically
    UIManager is used to change look and feel of java application
    dynamically. SwingUtilities.updateComponentTreeUI must be passed at
    least one component to update. All rest of the components are figured
    out by itself.

---
+++ src/com/fivedots/hello/LookAndFeelTest.java
@@ -0,0 +1,78 @@
+package com.fivedots.hello;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class LookAndFeelTest {
+	public static void main(String[] args){
+		EventQueue.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				LookAndFeelFrame frame = new LookAndFeelFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+				
+			}
+		});
+	}
+}
+
+class LookAndFeelFrame extends JFrame{
+	public LookAndFeelFrame(){
+		setTitle("LookAndFeel Test");
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		
+		buttonPanel = new JPanel();
+		add(buttonPanel);
+		
+		makeButton("Windows", "com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		makeButton("Motif", "com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+		makeButton("Metal", "javax.swing.plaf.metal.MetalLookAndFeel");
+		
+		
+				
+	}
+	
+	public void makeButton(String name, String themeName){
+		JButton button = new JButton(name);
+		buttonPanel.add(button);
+		
+		LookAndFeelChangeListener listener = new LookAndFeelChangeListener(themeName);
+		button.addActionListener(listener);
+	}
+	
+	class LookAndFeelChangeListener implements ActionListener{
+
+		public LookAndFeelChangeListener(String plaf){
+			theme = plaf;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			try {
+				UIManager.setLookAndFeel(theme);
+				SwingUtilities.updateComponentTreeUI(LookAndFeelFrame.this);
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+			
+		}
+		
+		private String theme;
+	}
+
+	
+	private JPanel buttonPanel;
+	
+	public static final int DEFAULT_WIDTH = 300;
+	public static final int DEFAULT_HEIGHT = 200;
+}
+
